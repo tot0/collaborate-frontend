ddb5cc9a80d7c9f5f6bbe4d63f573e7a4a2c82ce
Lucas Pickup <lucas.tot0.pickup@gmail.com>
2016-05-01 04:30:15 +1000

    COURSE PAGE!!!!!!

--- src/containers/CourseView/CourseView.js
+++ src/containers/CourseView/CourseView.js
@@ -4,6 +4,8 @@ import marked from 'marked'
 import styles from './CourseView.css'
 import {Segment, Content, Header, Rating, Grid, Column, Divider, Icon} from 'react-semantify'
 import { setCourse } from '../../actions/course'
+import OfferingCard from '../../components/OfferingCard/OfferingCard'
+
 class CourseView extends Component {
 
   componentWillMount() {
@@ -17,51 +19,77 @@ class CourseView extends Component {
     if (Object.keys(this.props.data).length === 0) {
       return null
     }
+    let results = null;
+    if(this.props.data.offerings.length) {
+      const sorted_results = this.props.data.offerings.sort(function(a, b) {
+        if (a.id < b.id) return -1
+        if (a.id > b.id) return 1
+        return 0
+      })
+      let self = this
+      results = this.props.data.offerings.map(function(offering) {
+        return <OfferingCard data={offering} key={offering.id} />
+      })
+    }
+
     return (
       <div>
-          <div className="ui top attached header">
-            <div className={styles.header}>
-              <div className={styles.code}>
-                {this.props.data.code}
+        <Grid>
+          <Column className="four wide" />
+          <Column className="eight wide">
+            <Header className="ui top attached header">
+              <div className={styles.header}>
+                <div className={styles.code}>
+                  {this.props.data.code}
+                </div>
+                <div className={styles.title}>
+                  {this.props.data.title}
+                </div>
               </div>
-              <div className={styles.title}>
-                {this.props.data.title}
-              </div>
-            </div>
-          </div>
-          <Segment className="bottom attached">
-            <Content>
-              <Grid>
-                <Column className="twelve wide">
-                  <div className="meta">
-                    <div className={styles.rating}>Semester 1:</div>
-                    <Rating className="star massive" init={{initialRating: this.props.data.ratings.sem_1.avg_rating, maxRating: 5, interactive: false}} disabled></Rating>
-                  </div>
-                </Column>
-                <Column className="four wide">
-                  <div>
-                    <Icon className={styles.thumb + " thumbs outline up big"} />
-                    <div className={styles.reccomend}>{this.props.data.ratings.sem_1.percent_reccomended}%</div>
-                  </div>
-                </Column>
-              </Grid>
-              <Divider className="" />
-              <Grid>
-                <Column className="twelve wide">
-                  <div className="meta">
-                    <div className={styles.rating}>Semester 2:</div>
-                    <Rating className="star massive" init={{initialRating: this.props.data.ratings.sem_2.avg_rating, maxRating: 5, interactive: false}} disabled></Rating>
-                  </div>
-                </Column>
-                <Column className="four wide">
-                  <div>
-                    <Icon className={styles.thumb + " thumbs outline up big"} />
-                    <div className={styles.reccomend}>{this.props.data.ratings.sem_2.percent_reccomended}%</div>
-                  </div>
-                </Column>
-              </Grid>
-            </Content>
-          </Segment>
+            </Header>
+            <Segment className="bottom attached">
+              <Content>
+                <Grid>
+                  <Column className="twelve wide">
+                    <div className="meta">
+                      <div className={styles.rating}>Semester 1:</div>
+                      <Rating className="star massive" init={{initialRating: this.props.data.ratings.sem_1.avg_rating, maxRating: 5, interactive: false}} disabled></Rating>
+                    </div>
+                  </Column>
+                  <Column className="four wide">
+                    <div>
+                      <Icon className={styles.thumb + " thumbs outline up big"} />
+                      <div className={styles.reccomend}>{this.props.data.ratings.sem_1.percent_reccomended}%</div>
+                    </div>
+                  </Column>
+                </Grid>
+                <Divider className="" />
+                <Grid>
+                  <Column className="twelve wide">
+                    <div className="meta">
+                      <div className={styles.rating}>Semester 2:</div>
+                      <Rating className="star massive" init={{initialRating: this.props.data.ratings.sem_2.avg_rating, maxRating: 5, interactive: false}} disabled></Rating>
+                    </div>
+                  </Column>
+                  <Column className="four wide">
+                    <div>
+                      <Icon className={styles.thumb + " thumbs outline up big"} />
+                      <div className={styles.reccomend}>{this.props.data.ratings.sem_2.percent_reccomended}%</div>
+                    </div>
+                  </Column>
+                </Grid>
+              </Content>
+            </Segment>
+          </Column>
+          <Column className="four wide" />
+        </Grid>
+        <Grid>
+          <Column className="four wide" />
+          <Column className="eight wide">
+            {results}
+          </Column>
+          <Column className="four wide" />
+        </Grid>
       </div>
     );
   }
