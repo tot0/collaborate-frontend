faa23bff78e1cc0a5fb06d712a0a1dba1088bbb1
Lucas Pickup <lucas.tot0.pickup@gmail.com>
2016-05-01 00:53:53 +1000

    Updated CourseView a whole lot.

--- src/components/CourseView/CourseView.js
+++ src/components/CourseView/CourseView.js
@@ -1,18 +1,54 @@
 import React from 'react'
 import marked from 'marked'
-import {Segment, Content, Header, Rating} from 'react-semantify'
+import styles from './CourseView.css'
+import {Segment, Content, Header, Rating, Grid, Column, Divider, Icon} from 'react-semantify'
 
 function CourseView(props) {
     return (
       <div>
-        <Segment className="fluid">
-          <Content>
-            <Header>{props.data.name} <small>{props.data.year}s{props.data.sem}</small></Header>
-            <div className="meta">
-              <Rating className="star" init={{initialRating: props.data.rating, maxRating: 5, interactive: false}} disabled></Rating>
+          <div className="ui top attached header">
+            <div className={styles.header}>
+              <div className={styles.code}>
+                {props.data.code}
+              </div>
+              <div className={styles.title}>
+                {props.data.title}
+              </div>
             </div>
-          </Content>
-        </Segment>
+          </div>
+          <Segment className="bottom attached">
+            <Content>
+              <Grid>
+                <Column className="twelve wide">
+                  <div className="meta">
+                    <div className={styles.rating}>Semester 1:</div>
+                    <Rating className="star massive" init={{initialRating: props.data.ratings.sem_1.avg_rating, maxRating: 5, interactive: false}} disabled></Rating>
+                  </div>
+                </Column>
+                <Column className="four wide">
+                  <div>
+                    <Icon className={styles.thumb + " thumbs outline up big"} />
+                    <div className={styles.reccomend}>{props.data.ratings.sem_1.percent_reccomended}%</div>
+                  </div>
+                </Column>
+              </Grid>
+              <Divider className="" />
+              <Grid>
+                <Column className="twelve wide">
+                  <div className="meta">
+                    <div className={styles.rating}>Semester 2:</div>
+                    <Rating className="star massive" init={{initialRating: props.data.ratings.sem_2.avg_rating, maxRating: 5, interactive: false}} disabled></Rating>
+                  </div>
+                </Column>
+                <Column className="four wide">
+                  <div>
+                    <Icon className={styles.thumb + " thumbs outline up big"} />
+                    <div className={styles.reccomend}>{props.data.ratings.sem_2.percent_reccomended}%</div>
+                  </div>
+                </Column>
+              </Grid>
+            </Content>
+          </Segment>
       </div>
     );
 }
